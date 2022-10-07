7760e1c0cabd4f3b4374ccde66a51c6cb2a0c1f0
Jayde 3 <93054701+BinaryCrown@users.noreply.github.com>
2022-10-07 13:27:58 +0100

    Fixed Ordinal class to use BPF rather than CNF

--- JS Libraries/Infinitae.js
+++ JS Libraries/Infinitae.js
@@ -1,5 +1,4 @@
-// A large number library which uses Cantor normal form and the Hardy hierarchy to store numbers up to H_e0(900719925474099)
-// Generalizes hardy-hierarchy.py by adding more methods.
+// A large number library which uses Buchholz's psi function and the SGH to store numbers up to g_BO(900719925474099)
 
 (function (globalScope) {
     "use strict";
@@ -10,8 +9,41 @@
         }
       
         constructor(expression) {
-            this.exponents = null;
-            this.coefficients = null;
+            let arr = expression.split("+");
+            if (arr.length == 1) {
+                arr = arr[0];
+                if (arr == "0") {
+                    this.type = 0;                  // Type 0: zero, type 1: sum, type 2: principal
+                    this.index = null;              // Index is null if not of type 2.
+                    this.summands = null;           // Summands is null if not of type 1/
+                    this.input = null;              // Input is null if not of type 2
+                }
+                else {
+                    narr = arr.split("ψ");
+                    narr.shift();
+                    for (let i = 0; i < narr.length; i++) {
+                        arr[i] = narr[i].split("(");
+                    }
+                    arr = narr.flat();
+                    this.type = 2;
+                    this.index = +narr[0];
+                    this.summands = null;
+                    str = arr[0].split("");
+                    str.shift();
+                    str.shift();
+                    str.shift();
+                    str.pop();
+                    str = str.join("");
+                    this.input = Ord(str);
+                }   
+            }
+            else {
+                this.type = 1;
+                this.index = null;
+                arr.shift();
+                this.summands = arr;
+                this.input = null;
+            }
         }
     }
 
